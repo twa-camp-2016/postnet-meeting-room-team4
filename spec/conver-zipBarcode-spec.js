3f93985dadea92e01f70e533a5d8b194ae9e8474
Liuhaiyan6 <2391348695@qq.com>
2016-08-05 09:21:08 +0800

    comit

--- spec/conver-zipBarcode-spec.js
+++ spec/conver-zipBarcode-spec.js
@@ -2,215 +2,92 @@
  * Created by amber on 16-8-2.
  */
 'use strict';
-let {judgeZipcodesLegality, addCheckcode, matchedBarcodes,converZipcodeBarcodes} =require("../src/conver-zipBarcode.js");
+let {
+    judgeZipcodesLegality, addCheckcode, matchedBarcodes, convertZipcodeBarcodes,
+    judgeBarcodesLegality, matchedZipcodes, formateResult, getZipcode, convertBarcodeToZipcode,
+} =require("../src/conver-zipBarcode.js");
 
 describe('judgeZipcodesLegality', function () {
-
     describe('judgeLength', function () {
-        it('should ensure length of zipcode is 5', function () {
-            let inputs = '12345';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 1;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should be 5', function () {
+            expect(judgeZipcodesLegality('12345')).toBeTruthy();
         });
-        it('should ensure length of zipcode is 9', function () {
-            let inputs = '123456789';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 1;
-            expect(zipcodesLegality).toEqual(expected);
-
+        it('should be 9', function () {
+            expect(judgeZipcodesLegality('123456789')).toBeTruthy();
         });
-        it('should ensure length of zipcode is 10', function () {
-            let inputs = '12345-6789';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 1;
-            expect(zipcodesLegality).toEqual(expected);
-
+        it('should be 10', function () {
+            expect(judgeZipcodesLegality('12345-6789')).toBeTruthy();
         });
-        it('should let  length of zipcode is 4', function () {
-            let inputs = '1234';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
 
+        it('should be 4', function () {
+            expect(judgeZipcodesLegality('1234')).toBeFalsy();
         });
-        it('should let length of zipcode is 6', function () {
-            let inputs = '123456';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-
+        it('should be 6', function () {
+            expect(judgeZipcodesLegality('123456')).toBeFalsy();
         });
-        it('should let length of zipcode is 8', function () {
-            let inputs = '12345678';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-
+        it('should be 8', function () {
+            expect(judgeZipcodesLegality('12345678')).toBeFalsy();
         });
-        it('should let length of zipcode is 11', function () {
-            let inputs = '11234567891';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-
-        })
+        it('should be 11', function () {
+            expect(judgeZipcodesLegality('12345678901')).toBeFalsy();
+        });
     });
 
     describe('judge location of ' - '', function () {
-        it('should let length is 2 and location is III', function () {
-            let inputs = '12-34';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should be 5', function () {
+            expect(judgeZipcodesLegality('1234-5678')).toBeFalsy();
         });
-        it('should let length is 9 and location is V', function () {
-            let inputs = '1234-5678';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should be 7', function () {
+            expect(judgeZipcodesLegality('123456-789')).toBeFalsy();
         });
-        it('should let length is 10 and location is VII', function () {
-            let inputs = '123456-789';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should let length is 10 and location is V', function () {
-            let inputs = '1234-56789';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should be 6', function () {
+            expect(judgeZipcodesLegality('12345-6789')).toBeTruthy();
         });
     });
 
     describe('judge counts of ' - '', function () {
-        it('should let length is 5 and countsOf' - ' is I', function () {
-            let inputs = '12-345';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should be 1', function () {
+            expect(judgeZipcodesLegality('12-345')).toBeFalsy();
         });
-        it('should let length is 5 and countsOf' - ' is II', function () {
-            let inputs = '12-3-';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should be 2', function () {
+            expect(judgeZipcodesLegality('12-3-')).toBeFalsy();
         });
-        it('should let length is 9 and countsOf' - ' is I', function () {
-            let inputs = '1234-5678';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should let length is 9 and countsOf' - ' is III', function () {
-            let inputs = '2-345-67-';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should let length is 9 and countsOf' - ' is I', function () {
-            let inputs = '1234-5678';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should let length is 10 and countsOf' - ' is II', function () {
-            let inputs = '12345-67-8';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should let length is 10 and countsOf' - ' is 0', function () {
-            let inputs = '1234567891';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should be 0', function () {
+            expect(judgeZipcodesLegality('1234567891')).toBeFalsy();
         });
     });
 
     describe('judge otherSpecial symbols', function () {
-        it('should let length is 5 and contain *', function () {
-            let inputs = '123*5';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should be *', function () {
+            expect(judgeZipcodesLegality('123*5')).toBeFalsy()
         });
-        it('should let length is 9 and contain ?', function () {
-            let inputs = '1234?5678';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should let length is 10 and contain &', function () {
-            let inputs = '12345&6789';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+        it('should  be a', function () {
+            expect(judgeZipcodesLegality('1234a5678')).toBeFalsy()
         });
     });
-
-    describe('judge other characters', function () {
-        it('should let length is 5 and contain a', function () {
-            let inputs = '123a5';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should let length is 9 and contain b&c', function () {
-            let inputs = '123b5678c';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should let length is 10 and contain dy', function () {
-            let inputs = '123d54678y';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-    });
-
-    describe(' judge multiple  style and length is 10', function () {
-        it('should has multiple  style', function () {
-            let inputs = '12*3a4-54-';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
+    describe(' judge multiple  style ', function () {
         it('should has multiple  style and length is 10', function () {
-            let inputs = '1234b-78*1';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+            expect(judgeZipcodesLegality('1234b-78*1')).toBeFalsy()
         });
         it('should has multiple  style and length is 5', function () {
-            let inputs = '1*b-3';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
-        });
-        it('should has multiple  style and length is 9', function () {
-            let inputs = '345*&d543';
-            let zipcodesLegality = judgeZipcodesLegality(inputs);
-            let expected = 0;
-            expect(zipcodesLegality).toEqual(expected);
+            expect(judgeZipcodesLegality('1*b-3')).toBeFalsy()
         });
     });
-
 });
-
-
-
 describe('addCheckcode', function () {
-    it('should ensure length of zipcode is 5', function () {
-        let inputs = '12345';
-        let updateZipcodes = addCheckcode(inputs);
+    it('should be 5', function () {
+        let inputZip = '12345';
+        let updateZipcodes = addCheckcode(inputZip);
         let expected = '123455';
         expect(updateZipcodes).toEqual(expected);
     });
+    it('should be 0', function () {
+        let inputZip = '123456784';
+        let updateZipcodes = addCheckcode(inputZip);
+        let expected = '1234567840';
+        expect(updateZipcodes).toEqual(expected);
+    });
 });
-
 describe('matchedBarcodes', function () {
     it('should ensure length of zipcode is 5', function () {
         let updateZipcodes = '957135';
@@ -218,7 +95,7 @@ describe('matchedBarcodes', function () {
         let expected = '||:|:::|:|:|:::|:::||::||::|:|:|';
         expect(barcodes).toEqual(expected);
     });
-    it('should ensure length of zipcode is 5', function () {
+    it('should ensure length of zipcode is 9', function () {
         let updateZipcodes = '450512346';
         let barcodes = matchedBarcodes(updateZipcodes);
         let expected = '|:|::|:|:|:||::::|:|::::||::|:|::||::|::|:||::|';
@@ -229,31 +106,158 @@ describe('matchedBarcodes', function () {
         let barcodes = matchedBarcodes(updateZipcodes);
         let expected = '|:|::|:|:|:||::::|:|::::||::|:|::||::|::|:||::|';
         expect(barcodes).toEqual(expected);
-
     });
 });
-
-describe('converZipcodeBarcodes', function () {
+describe('convertZipcodeBarcodes', function () {
     it('should ensure length of zipcode is 5', function () {
-        let inputs = '95713';
-        let barcodes = converZipcodeBarcodes(inputs);
+        let inputZip = '95713';
+        let barcodes = convertZipcodeBarcodes(inputZip);
         let expected = '||:|:::|:|:|:::|:::||::||::|:|:|';
         expect(barcodes).toEqual(expected);
     });
     it('should ensure length of zipcode is 9', function () {
-        let inputs = '45051234';
-        let barcodes = converZipcodeBarcodes(inputs);
-        let expected = '|:|::|:|:|:||::::|:|::::||::|:|::||::|::|:||::|';
+        let inputZip = '450512349';
+        let barcodes = convertZipcodeBarcodes(inputZip);
+        let expected = '|:|::|:|:|:||::::|:|::::||::|:|::||::|::||:|::::||:|';
         expect(barcodes).toEqual(expected);
-
     });
     it('should ensure length of zipcode is 10', function () {
-        let inputs = '45056-1234';
-        let barcodes = converZipcodeBarcodes(inputs);
-        let expected = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|:||::|';
+        let inputZip = '45056-1234';
+        let barcodes = convertZipcodeBarcodes(inputZip);
+        let expected = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
         expect(barcodes).toEqual(expected);
-
     });
 });
+
+describe('judgeBarcodesLegality', function () {
+    describe('inputs correctBarcodes length ', function () {
+
+        it('should be 32', function () {
+            expect(judgeBarcodesLegality('||:|:::|:|:|:::|:::||::||::|:|:|')).toBeTruthy();
+        });
+        it('should be 52', function () {
+            expect(judgeBarcodesLegality('|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|')).toBeTruthy();
+        });
+    });
+    describe('judgeSpecialSymbols', function () {
+        it('should judge whether or not special Symbols  52', function () {
+            expect(judgeBarcodesLegality('|:*::|:|:|:||::::|:|::|?:::::||::|:|::||a:|::|| :::|')).toBeFalsy();
+        });
+        it('should judge whether or not special Symbols  32', function () {
+            expect(judgeBarcodesLegality('||:a:::|:|:#:::|:::||::4|::|: :|')).toBeFalsy();
+        });
+    });
+    describe('should judge whether or not barcodes of length is 32 or 52', function () {
+        it('input length of barcodes 30', function () {
+            expect(judgeBarcodesLegality('||:|:::|:|:|:::|:::||::||::|')).toBeFalsy();
+        });
+        it('input length of barcodes 53', function () {
+            expect(judgeBarcodesLegality('|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|||')).toBeFalsy();
+        });
+    });
+    describe('should judge whether or not bothsideLongLine', function () {
+        it('Right is not bothsideLongLine ', function () {
+            expect(judgeBarcodesLegality('||:|:::|:|:|:::|:::||::||::|:|::')).toBeFalsy();
+        });
+        it('Left is not bothsideLongLine ', function () {
+            expect(judgeBarcodesLegality(':|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::||')).toBeFalsy();
+        });
+    });
+
+    describe('should judge whether or not elment of charArray has || and ::: ', function () {
+        it('elment of charArray has :::: | ', function () {
+            expect(judgeBarcodesLegality('|::::::|:|:|:::|:::||::||::|:|:|')).toBeFalsy();
+        });
+        it('elment of charArray has :: |||', function () {
+            expect(judgeBarcodesLegality('|:||:|:|:|:||::::|:|::||:::::||::|:|::||::|::|||||||')).toBeFalsy();
+        });
+    });
+});
+
+describe('should judge correct', function () {
+    it('multiple', function () {
+        expect('||::::*|:|:|:: :::||::||::|:|:').toBeFalsy();
+    });
+    it('elment of charArray has :: |||', function () {
+        expect('|:*|:|:|:|:||::::?:|::|b:::::||::|:|::||::|::||||::|').toBeFalsy();
+    });
+});
+
+
+describe('matchedZipcodes', function () {
+    it('should input correctBarcodes of 32', function () {
+        let inputs = '||:|:::|:|:|:::|:::||::||::|:|:|';
+        let partZipcodes = matchedZipcodes(inputs);
+        let expected = '957135';
+        expect(partZipcodes).toEqual(expected);
+    });
+    it('should input correctBarcodes of 52', function () {
+        let inputs = '|:|::|:|:|:||::::|:|::::||::|:|::||::|::|:||::|';
+        let partZipcodes = matchedZipcodes(inputs);
+        let expected = '450512346';
+        expect(partZipcodes).toEqual(expected);
+    });
+});
+describe('judgeZipcode', function () {
+    it('should input correctBarcodes of 32', function () {
+        let partZipcodes = '957135';
+        let formateBarcode = formateResult(PartBarcode);
+        let expected = true;
+        expect(partZipcodes).toEqual(expected);
+    });
+    it('should input correctBarcodes of 52', function () {
+        let partZipcodes = '450512346';
+        let formateBarcode = formateResult(PartBarcode);
+        let expected = true;
+        expect(partZipcodes).toEqual(expected);
+    });
+});
+
+describe('judgeZipcodelegality', function () {
+
+    it('5 位长度', function () {
+        let partZipcodes = '957135';
+        let zipcodesLast = getZipcode(PartBarcode);
+        let expected = '95713';
+        expect(zipcodesLast).toEqual(expected);
+    });
+    it('9位长度', function () {
+        let partZipcodes = '450512346';
+        let zipcodesLast = getZipcode(PartBarcode);
+        let expected = '450512346';
+        expect(zipcodesLast).toEqual(expected);
+    });
+    it('10 位长度', function () {
+        let partZipcodes = '450512346';
+        let zipcodesLast = getZipcode(PartBarcode);
+        let expected = '45051-2346';
+        expect(zipcodesLast).toEqual(expected);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
